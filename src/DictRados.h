9eed432d6fe768de896ae63c9ab9aef83b1dbc7e
Peter Burow <p.burow@tallence.com>
2017-03-21 11:51:28 +0100

    Cluster connect and io_ctx creation added

--- src/DictRados.h
+++ src/DictRados.h
@@ -15,11 +15,11 @@ int rados_dict_init(struct dict *driver, const char *uri, const struct dict_sett
 		const char **error_r);
 void rados_dict_deinit(struct dict *_dict);
 
-int rados_dict_lookup(struct dict *_dict, pool_t pool, const char *key, const char **value_r, const char **error_r);
+int rados_dict_lookup(struct dict *_dict, pool_t pool, const char *key, const char **value_r);
 int rados_dict_lookup_async(struct dict *_dict, const char *key, dict_lookup_callback_t *callback, void *context);
 
 struct dict_transaction_context *rados_transaction_init(struct dict *_dict);
-void rados_transaction_commit(struct dict_transaction_context *_ctx, bool async, dict_transaction_commit_callback_t *callback,
+int rados_transaction_commit(struct dict_transaction_context *_ctx, bool async, dict_transaction_commit_callback_t *callback,
 		void *context);
 void rados_transaction_rollback(struct dict_transaction_context *_ctx);
 
@@ -29,6 +29,6 @@ void rados_atomic_inc(struct dict_transaction_context *_ctx, const char *key, lo
 
 struct dict_iterate_context *rados_dict_iterate_init(struct dict *_dict, const char * const *paths, enum dict_iterate_flags flags);
 bool rados_dict_iterate(struct dict_iterate_context *ctx, const char **key_r, const char **value_r);
-int rados_dict_iterate_deinit(struct dict_iterate_context *ctx, const char **error_r);
+int rados_dict_iterate_deinit(struct dict_iterate_context *ctx);
 
 #endif /* SRC_DICTRADOS_H_ */
--- src/DictRados.hpp
+++ src/DictRados.hpp
@@ -1,10 +1,28 @@
 #ifndef SRC_DICTRADOS_HPP_
 #define SRC_DICTRADOS_HPP_
 
+#include <rados/librados.hpp>
+
 class DictRados {
+
+private:
+	librados::Rados cluster;
+	librados::IoCtx io_ctx;
+
 public:
 	DictRados();
 	virtual ~DictRados();
+
+	int initCluster(const char *const name, const char *const clustername, uint64_t flags);
+	int readConfigFile(const char *path);
+	int parseArguments(int argc, const char **argv);
+	int connect();
+	void shutdown();
+
+	int createIOContext(const char *name);
+	void closeIOContext();
+	void setIOContextNamespace(const char *nspace);
+
 };
 
 #endif /* SRC_DICTRADOS_HPP_ */
