e0c54ae9003b598fe17958e1d89616f60a113783
Peter Burow <h.burow@tallence.com>
2017-03-23 11:28:30 +0100

    struct rados_dict members moved to DictRados class

--- src/DictRados.hpp
+++ src/DictRados.hpp
@@ -9,18 +9,93 @@ private:
 	librados::Rados cluster;
 	librados::IoCtx io_ctx;
 
+	std::string sPool;
+	std::string sOid;
+	std::string sConfig;
+	std::string sUsername;
+	std::string sClusterName;
+	std::string sClusterUser;
+
+	std::map<std::string, librados::bufferlist> readerMap;
+	typename std::map<std::string, librados::bufferlist>::iterator readerMapIter;
+
 public:
 	DictRados();
 	virtual ~DictRados();
 
 	int initCluster(const char *const name, const char *const clustername, uint64_t flags);
-	int readConfigFile(const char *path);
+	int readConfigFile(const char * path);
 	int parseArguments(int argc, const char **argv);
 	int connect();
 	void shutdown();
 
 	int createIOContext(const char *name);
 	librados::IoCtx* getIOContext() {return &io_ctx;}
+
+	void clearReaderMap();
+	void incrementReaderMapIterator();
+	void beginReaderMapIterator();
+	bool isEndReaderMapIterator();
+
+	const std::string& getConfig() const {
+		return sConfig;
+	}
+
+	void setConfig(const std::string& config) {
+		sConfig = config;
+	}
+
+	const std::string& getOid() const {
+		return sOid;
+	}
+
+	void setOid(const std::string& oid) {
+		sOid = oid;
+	}
+
+	const std::string& getPool() const {
+		return sPool;
+	}
+
+	void setPool(const std::string& pool) {
+		sPool = pool;
+	}
+
+	const std::string& getUsername() const {
+		return sUsername;
+	}
+
+	void setUsername(const std::string& username) {
+		sUsername = username;
+	}
+
+	const std::string& getClusterName() const {
+		return sClusterName;
+	}
+
+	void setClusterName(const std::string& clusterName) {
+		sClusterName = clusterName;
+	}
+
+	const std::string& getClusterUser() const {
+		return sClusterUser;
+	}
+
+	void setClusterUser(const std::string& clusterUser) {
+		sClusterUser = clusterUser;
+	}
+
+	const std::map<std::string, librados::bufferlist>& getReaderMap() const {
+		return readerMap;
+	}
+
+	std::map<std::string, librados::bufferlist>& getReaderMap() {
+		return readerMap;
+	}
+
+	typename std::map<std::string, librados::bufferlist>::iterator getReaderMapIter() const {
+		return readerMapIter;
+	}
 
 };
 
