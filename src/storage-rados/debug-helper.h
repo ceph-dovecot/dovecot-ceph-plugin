85a31136b268599e021586b02d65187897990307
Peter Burow <h.burow@tallence.com>
2017-04-11 14:27:30 +0200

    Print debug infos

---
+++ src/storage-rados/debug-helper.h
@@ -0,0 +1,15 @@
+/*
+ * debug-helper.h
+ *
+ *  Created on: Apr 11, 2017
+ *      Author: peter
+ */
+
+#ifndef DEBUG_HELPER_H_
+#define DEBUG_HELPER_H_
+
+void debug_print_mail(struct mail *mail, const char *funcname);
+void debug_print_mailbox(struct mailbox *mailbox, const char *funcname);
+void debug_print_index_mail_data(struct index_mail_data *indexMailData, const char *funcname);
+
+#endif /* DEBUG_HELPER_H_ */
---
+++ src/storage-rados/test-debug-helper.c
@@ -0,0 +1,40 @@
+/*
+ * test-debug-helper.c
+ *
+ *  Created on: Apr 11, 2017
+ *      Author: peter
+ */
+
+#include "lib.h"
+#include "failures.h"
+#include "index-mail.h"
+#include "mail-storage.h"
+#include "mail-storage-private.h"
+#include "debug-helper.h"
+
+int main(int argc, char **argv) {
+	int ret = 0;
+
+	struct mail *mail = i_new(struct mail, 1);
+	struct mailbox *mailbox = i_new(struct mailbox, 1);
+	struct index_mail_data *index_mail_data = i_new(struct index_mail_data, 1);
+
+	mailbox->name = i_strdup("hburow");
+	mailbox->flags = MAILBOX_FLAG_READONLY | MAILBOX_FLAG_KEEP_LOCKED;
+	mailbox->open_error = MAIL_ERROR_EXISTS;
+
+	mail->box = mailbox;
+	mail->uid = 123;
+
+	debug_print_mail(mail, "test-debug-helper::main()");
+	debug_print_mail(mail, "test-debug-helper::test()");
+	debug_print_index_mail_data(index_mail_data, "test-debug-helper::main()");
+
+	i_free(mail->box);
+	i_free(mail);
+
+	return ret;
+
+}
+
+
