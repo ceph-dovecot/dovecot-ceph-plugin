2495b877c255817d6f78f572ae8d3216d3d3ddce
Peter Burow <h.burow@tallence.com>
2017-04-12 09:18:25 +0200

    More debug infos

--- src/storage-rados/debug-helper.h
+++ src/storage-rados/debug-helper.h
@@ -11,5 +11,6 @@
 void debug_print_mail(struct mail *mail, const char *funcname);
 void debug_print_mailbox(struct mailbox *mailbox, const char *funcname);
 void debug_print_index_mail_data(struct index_mail_data *indexMailData, const char *funcname);
+void debug_print_mail_save_context(struct mail_save_context *mailSaveContext, const char *funcname);
 
 #endif /* DEBUG_HELPER_H_ */
