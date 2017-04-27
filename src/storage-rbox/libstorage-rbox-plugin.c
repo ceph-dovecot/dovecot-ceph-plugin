374b5da0d02f7d264eb3ef702163379a8871f0cb
Peter Mauritius <p.mauritius@tallence.com>
2017-04-27 18:34:42 +0200

    new rbox module based on sdbox created

---
+++ src/storage-rbox/libstorage-rbox-plugin.c
@@ -0,0 +1,30 @@
+
+
+#include "lib.h"
+#include "config.h"
+#include "module-context.h"
+#include "mail-storage.h"
+
+#include "libstorage-rbox-plugin.h"
+
+extern struct mail_storage rbox_storage;
+
+static int refcount = 0;
+
+void storage_rbox_plugin_init(struct module *module) {
+	i_debug("storage_rbox_plugin_init refcount=%d", refcount);
+	if (refcount++ > 0)
+		return;
+	i_debug("storage_rbox_plugin_init registers rbox_storage ");
+	mail_storage_class_register(&rbox_storage);
+}
+
+void storage_rbox_plugin_deinit(void) {
+	i_debug("storage_rbox_plugin_deinit refcount=%d)", refcount);
+	if (--refcount > 0)
+		return;
+	i_debug("storage_rbox_plugin_deinit unregisters rbox_storage ");
+	mail_storage_class_unregister(&rbox_storage);
+}
+
+const char *storage_rbox_plugin_version = DOVECOT_ABI_VERSION;
