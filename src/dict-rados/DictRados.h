fb55a6f116faa5b49e0b5bf7d413eb2371374ca8
Peter Mauritius <p.mauritius@tallence.com>
2017-05-11 13:18:01 +0200

    dict-rados formatted according to style guide

--- src/dict-rados/DictRados.h
+++ src/dict-rados/DictRados.h
@@ -1,31 +1,33 @@
-#ifndef SRC_DICTRADOS_H_
-#define SRC_DICTRADOS_H_
+#ifndef DOVECOT_RADOS_PLUGIN_DICT_RADOS_DICTRADOS_H_
+#define DOVECOT_RADOS_PLUGIN_DICT_RADOS_DICTRADOS_H_
 
 #include "lib.h"
 #include "dict-private.h"
 
 extern int rados_dict_init(struct dict *driver, const char *uri, const struct dict_settings *set, struct dict **dict_r,
-		const char **error_r);
+                           const char **error_r);
 extern void rados_dict_deinit(struct dict *_dict);
 
 extern int rados_dict_wait(struct dict *dict);
 
 extern int rados_dict_lookup(struct dict *_dict, pool_t pool, const char *key, const char **value_r);
-extern void rados_dict_lookup_async(struct dict *_dict, const char *key, dict_lookup_callback_t *callback, void *context);
+extern void rados_dict_lookup_async(struct dict *_dict, const char *key, dict_lookup_callback_t *callback,
+                                    void *context);
 
 extern struct dict_transaction_context *rados_transaction_init(struct dict *_dict);
-extern int rados_transaction_commit(struct dict_transaction_context *_ctx, bool async, dict_transaction_commit_callback_t *callback,
-		void *context);
+extern int rados_transaction_commit(struct dict_transaction_context *_ctx, bool async,
+                                    dict_transaction_commit_callback_t *callback, void *context);
 extern void rados_transaction_rollback(struct dict_transaction_context *_ctx);
 
 extern void rados_set(struct dict_transaction_context *_ctx, const char *key, const char *value);
 extern void rados_unset(struct dict_transaction_context *_ctx, const char *key);
 extern void rados_atomic_inc(struct dict_transaction_context *_ctx, const char *key, long long diff);
 
-extern struct dict_iterate_context *rados_dict_iterate_init(struct dict *_dict, const char * const *paths,
-		enum dict_iterate_flags flags);
+extern struct dict_iterate_context *rados_dict_iterate_init(struct dict *_dict, const char *const *paths,
+                                                            enum dict_iterate_flags flags);
 extern bool rados_dict_iterate(struct dict_iterate_context *ctx, const char **key_r, const char **value_r);
 extern int rados_dict_iterate_deinit(struct dict_iterate_context *ctx);
 
 extern void rados_dict_set_timestamp(struct dict_transaction_context *ctx, const struct timespec *ts);
-#endif /* SRC_DICTRADOS_H_ */
+
+#endif /* DOVECOT_RADOS_PLUGIN_DICT_RADOS_DICTRADOS_H_ */
