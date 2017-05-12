ea7b44562aaac582fa8eb78d09eda00632f373a6
Peter Mauritius <p.mauritius@tallence.com>
2017-05-12 16:10:03 +0200

    seperated C++ class und C binding

--- src/dict-rados/libdict-rados-plugin.h
+++ src/dict-rados/libdict-rados-plugin.h
@@ -1,11 +1,11 @@
 /* Copyright (c) 2017 Tallence AG and the authors, see the included COPYING file */
 
-#ifndef DOVECOT_RADOS_PLUGIN_DICT_RADOS_LIBDICT_RADOS_PLUGIN_H_
-#define DOVECOT_RADOS_PLUGIN_DICT_RADOS_LIBDICT_RADOS_PLUGIN_H_
+#ifndef SRC_DICT_RADOS_LIBDICT_RADOS_PLUGIN_H_
+#define SRC_DICT_RADOS_LIBDICT_RADOS_PLUGIN_H_
 
 struct module;
 
 void dict_rados_plugin_init(struct module *module);
 void dict_rados_plugin_deinit(void);
 
-#endif /* DOVECOT_RADOS_PLUGIN_DICT_RADOS_LIBDICT_RADOS_PLUGIN_H_ */
+#endif /* SRC_DICT_RADOS_LIBDICT_RADOS_PLUGIN_H_ */
