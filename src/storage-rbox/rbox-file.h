69345f19e3ec237c23fa57edaa646558953ea072
Peter Mauritius <p.mauritius@tallence.com>
2017-05-28 16:12:46 +0200

    removed segv on mail save, write metadata to omap

--- src/storage-rbox/rbox-file.h
+++ src/storage-rbox/rbox-file.h
@@ -6,6 +6,30 @@
 
 #include "dbox-file.h"
 
+/* Globally unique identifier for the message. Preserved when
+   copying. */
+#define RBOX_METADATA_ALL "METADATA"
+#define RBOX_METADATA_GUID "G"
+/* POP3 UIDL overriding the default format */
+#define RBOX_METADATA_POP3_UIDL "P"
+/* POP3 message ordering (for migrated mails) */
+#define RBOX_METADATA_POP3_ORDER "O"
+/* Received UNIX timestamp in hex */
+#define RBOX_METADATA_RECEIVED_TIME "R"
+/* Physical message size in hex. Necessary only if it differs from
+   the dbox_message_header.message_size_hex, for example because the
+   message is compressed. */
+#define RBOX_METADATA_PHYSICAL_SIZE "Z"
+/* Virtual message size in hex (line feeds counted as CRLF) */
+#define RBOX_METADATA_VIRTUAL_SIZE "V"
+/* Pointer to external message data. Format is:
+   1*(<start offset> <byte count> <options> <ref>) */
+#define RBOX_METADATA_EXT_REF "X"
+/* Mailbox name where this message was originally saved to.
+   When rebuild finds a message whose mailbox is unknown, it's
+   placed to this mailbox. */
+#define RBOX_METADATA_ORIG_MAILBOX "B"
+
 struct rbox_file {
   struct dbox_file file;
   struct rbox_mailbox *mbox;
