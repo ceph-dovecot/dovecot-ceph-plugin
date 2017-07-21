e790449f8754fe3696a130dbbdb148c5327ccadc
jan <j.radon@tallence.com>
2017-07-21 17:07:37 +0200

    moved several sync-context related functions (based on sdbox) to the project. Enhanced the rbox-sync code with sdbbox sync handling.

--- src/storage-rbox/rbox-storage.hpp
+++ src/storage-rbox/rbox-storage.hpp
@@ -25,15 +25,14 @@ struct rbox_storage {
 };
 
 #endif
-
+extern void rbox_sync_update_header(struct index_rebuild_context *ctx);
 extern struct mail_vfuncs rbox_mail_vfuncs;
-
+extern uint32_t rbox_get_uidvalidity_next(struct mailbox_list *list);
 extern struct mail_save_context *rbox_save_alloc(struct mailbox_transaction_context *_t);
 extern int rbox_save_begin(struct mail_save_context *ctx, struct istream *input);
 extern int rbox_save_continue(struct mail_save_context *ctx);
 extern int rbox_save_finish(struct mail_save_context *ctx);
 extern void rbox_save_cancel(struct mail_save_context *ctx);
-
 extern int rbox_transaction_save_commit_pre(struct mail_save_context *ctx);
 extern void rbox_transaction_save_commit_post(struct mail_save_context *ctx,
                                               struct mail_index_transaction_commit_result *result);
@@ -46,6 +45,11 @@ extern int rbox_mailbox_get_metadata(struct mailbox *box, enum mailbox_metadata_
 
 extern struct mailbox_sync_context *rbox_storage_sync_init(struct mailbox *box, enum mailbox_sync_flags flags);
 extern void rbox_notify_changes(struct mailbox *box);
+extern int rbox_read_header(struct rbox_mailbox *mbox, struct sdbox_index_header *hdr, bool log_error,
+                            bool *need_resize_r);
+
+extern int rbox_mailbox_create_indexes(struct mailbox *box, const struct mailbox_update *update,
+                                       struct mail_index_transaction *trans);
 
 struct mail_save_context *rbox_save_alloc(struct mailbox_transaction_context *_t);
 int rbox_save_begin(struct mail_save_context *ctx, struct istream *input);
