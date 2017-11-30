7a8d76a61807b38af31e78f4ad988e78024db87f
Peter Burow <p.burow@tallence.com>
2017-11-30 15:26:11 +0100

    Check of path added

--- src/scripts/delete_user.sh
+++ src/scripts/delete_user.sh
@@ -4,7 +4,6 @@
 
 # Loads utils script
 script_path=${0%/*}
-echo "script_path = $script_path"
 source $script_path/utils.sh
 
 # Reads mail_location of user
@@ -18,12 +17,14 @@ sep='/'
 IFS=$'\n'; mbox_array=($list); unset IFS;
 for mbox in "${mbox_array[@]}"; do
 	if ! string_contains $mbox $sep; then
-		echo "Delete $mbox"
 		delete_mailbox $1 $mbox || { echo "Delete $mbox failed" ; exit 1 ; } 
 	fi
 done
 
 # Delete mail_location
-echo "Delete $mail_location"
+if [ -z $mail_location ] || [ ! -d $mail_location ] ; then
+	error_exit "user path is empty or doesn't exist: $mail_location"								
+fi
+
 rm -Rf $mail_location
 echo $?
