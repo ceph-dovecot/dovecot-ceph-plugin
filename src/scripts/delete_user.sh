c6c1974ba2930ed6ffe36400f18ea1561fb3f4c0
Peter Burow <p.burow@tallence.com>
2017-12-01 11:19:11 +0100

    More checks

--- src/scripts/delete_user.sh
+++ src/scripts/delete_user.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-# main
+# Removes all mailboxes an the mail location of the given user
+#
+# Parameter $1 user id
+# Parameter $2 result of the move process
+#
+user=$1
+result=$2
 
-# Loads utils script
+# Load utility script
 script_path=${0%/*}
 source $script_path/utils.sh
 
+if [ ! $# -eq 2 ] ; then
+	 error_exit "Wrong number of arguments. Usage: $0 <user id> <result>"
+fi
+	  
+if [ ! "$2" = "0" ] ; then
+	 error_exit "Result of move process not 0"
+fi
+
 # Reads mail_location of user
-mail_location=$(get_user_mail_location $1)
+mail_location=$(get_user_mail_location $user)
 
 # Get all mailboxes
-list=$(get_mailbox_list $1)
-
+list=$(get_mailbox_list $user)
+		
 # Delete all mailboxes (exclude sub-boxes)
 sep='/'
 IFS=$'\n'; mbox_array=($list); unset IFS;
 for mbox in "${mbox_array[@]}"; do
 	if ! string_contains $mbox $sep; then
-		delete_mailbox $1 $mbox || { echo "Delete $mbox failed" ; exit 1 ; } 
+		delete_mailbox $user $mbox || { echo "Delete $mbox failed" ; exit 1 ; } 
 	fi
 done
 
