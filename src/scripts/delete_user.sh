57134ba0b03a022edc22a38213d756b69f51ceac
Peter Burow <p.burow@tallence.com>
2017-12-07 10:58:57 +0100

    Use doveadm.sh as subshell

--- src/scripts/delete_user.sh
+++ src/scripts/delete_user.sh
@@ -11,12 +11,13 @@ result=$2
 # Load utility script
 script_path=${0%/*}
 source "$script_path/utils.sh"
+source "$script_path/doveadm.sh"
 
-if [ ! $# -eq 2 ] ; then
+if [ ! $# -eq 2 ]; then
 	 error_exit "Wrong number of arguments. Usage: $0 <user id> <result>"
 fi
 	  
-if [ ! "$result" = "0" ] ; then
+if [ ! "$result" = "0" ]; then
 	 error_exit "Result of move process not 0"
 fi
 
@@ -27,7 +28,7 @@ list=$(get_mailbox_list "$user")
 sep='/'
 IFS=$'\n'; mbox_array=($list); unset IFS;
 for mbox in "${mbox_array[@]}"; do
-	if ! string_contains "$mbox" "$sep" ; then
+	if ! string_contains "$mbox" "$sep"; then
 		delete_mailbox "$user" "$mbox" || { echo "Delete $mbox failed" ; exit 1 ; } 
 	fi
 done
@@ -36,7 +37,7 @@ done
 mail_location=$(get_user_mail_location "$user")
 
 # Delete mail_location
-if [[ -z "$mail_location" ]] || [[ ! -d "$mail_location" ]] ; then
+if [[ -z "$mail_location" ]] || [[ ! -d "$mail_location" ]]; then
 	error_exit "user path is empty or doesn't exist: $mail_location"								
 fi
 rm -Rf "$mail_location"
--- src/scripts/move_to_mdbox.sh
+++ src/scripts/move_to_mdbox.sh
@@ -11,6 +11,7 @@ dest_path="$2"
 # Load utility script
 script_path=${0%/*}
 source "$script_path/utils.sh"
+source "$script_path/doveadm.sh"
 
 # Read mail_location of user
 mail_location=$(get_user_mail_location "$user")
