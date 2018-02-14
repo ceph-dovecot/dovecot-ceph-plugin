16fd9714cb9ed9d3eb2c4e027ee6fdb80ba1a290
Peter Burow <p.burow@tallence.com>
2018-02-14 08:55:23 +0100

    find_and_replace() added

--- src/librmb/rados-util.h
+++ src/librmb/rados-util.h
@@ -13,9 +13,11 @@
 #define SRC_LIBRMB_RADOS_UTIL_H_
 
 #include <string.h>
-#include <string>
-#include "time.h"
+#include <time.h>
 #include <stdlib.h>
+
+#include <string>
+
 #include "rados-types.h"
 
 namespace librmb {
@@ -27,14 +29,16 @@ class RadosUtils {
 
   static bool convert_str_to_time_t(const std::string &date, time_t *val);
   static bool is_numeric(const std::string &s);
-  static bool is_date_attribute(rbox_metadata_key &key);
+  static bool is_date_attribute(const rbox_metadata_key &key);
 
   static bool convert_string_to_date(const std::string &date_string, std::string *date);
   static int convert_time_t_to_str(const time_t &t, std::string *ret_val);
   static bool flags_to_string(const uint8_t &flags, std::string *flags_str);
   static bool string_to_flags(const std::string &flags_str, uint8_t *flags);
+
+  static void find_and_replace(std::string *source, std::string const &find, std::string const &replace);
 };
 
-} /* namespace tallence */
+}  // namespace librmb
 
-#endif /* SRC_LIBRMB_RADOS_UTIL_H_ */
+#endif  // SRC_LIBRMB_RADOS_UTIL_H_
