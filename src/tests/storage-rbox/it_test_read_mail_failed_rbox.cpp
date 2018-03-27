46e6a053f26a38acc67e6685bf1701658c3059d1
Jan Radon <j.radon@tallence.com>
2018-03-27 10:12:38 +0200

    added new unit tests (read mail fails)

---
+++ src/tests/storage-rbox/it_test_read_mail_failed_rbox.cpp
@@ -0,0 +1,143 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Copyright (c) 2017-2018 Tallence AG and the authors
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ */
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "TestCase.h"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshadow"           // turn off warnings for Dovecot :-(
+#pragma GCC diagnostic ignored "-Wundef"            // turn off warnings for Dovecot :-(
+#pragma GCC diagnostic ignored "-Wredundant-decls"  // turn off warnings for Dovecot :-(
+#ifndef __cplusplus
+#pragma GCC diagnostic ignored "-Wdeclaration-after-statement"  // turn off warnings for Dovecot :-(
+#endif
+
+extern "C" {
+#include "lib.h"
+#include "mail-user.h"
+#include "mail-storage.h"
+#include "mail-storage-service.h"
+#include "mail-namespace.h"
+#include "mailbox-list.h"
+#include "ioloop.h"
+#include "istream.h"
+#include "mail-search-build.h"
+
+#include "libdict-rados-plugin.h"
+#include "mail-search-parser-private.h"
+#include "mail-search.h"
+}
+#include "rbox-storage.hpp"
+#include "../mocks/mock_test.h"
+#include "dovecot-ceph-plugin-config.h"
+#include "../test-utils/it_utils.h"
+#include "rbox-mail.h"
+using ::testing::AtLeast;
+using ::testing::Return;
+
+TEST_F(StorageTest, init) {}
+
+TEST_F(StorageTest, mailbox_open_inbox) {
+  struct mail_namespace *ns = mail_namespace_find_inbox(s_test_mail_user->namespaces);
+  struct mailbox *box = mailbox_alloc(ns->list, "INBOX", MAILBOX_FLAG_READONLY);
+  ASSERT_GE(mailbox_open(box), 0);
+  mailbox_free(&box);
+}
+
+TEST_F(StorageTest, mail_copy_mail_in_inbox) {
+  struct mailbox_transaction_context *desttrans;
+  struct mail_save_context *save_ctx;
+  struct mail *mail;
+  struct mail_search_context *search_ctx;
+  struct mail_search_args *search_args;
+  struct mail_search_arg *sarg;
+
+  const char *message =
+      "From: user@domain.org\n"
+      "Date: Sat, 24 Mar 2017 23:00:00 +0200\n"
+      "Mime-Version: 1.0\n"
+      "Content-Type: text/plain; charset=us-ascii\n"
+      "\n"
+      "body\n";
+
+  const char *mailbox = "INBOX";
+
+  // testdata
+  testutils::ItUtils::add_mail(message, mailbox, StorageTest::s_test_mail_user->namespaces);
+
+  search_args = mail_search_build_init();
+  sarg = mail_search_build_add(search_args, SEARCH_ALL);
+  ASSERT_NE(sarg, nullptr);
+
+  struct mail_namespace *ns = mail_namespace_find_inbox(s_test_mail_user->namespaces);
+  ASSERT_NE(ns, nullptr);
+
+  struct mailbox *box = mailbox_alloc(ns->list, mailbox, MAILBOX_FLAG_SAVEONLY);
+
+  if (mailbox_open(box) < 0) {
+    i_error("Opening mailbox %s failed: %s", mailbox, mailbox_get_last_internal_error(box, NULL));
+    FAIL() << " Forcing a resync on mailbox INBOX Failed";
+  }
+#ifdef DOVECOT_CEPH_PLUGIN_HAVE_MAIL_STORAGE_TRANSACTION_OLD_SIGNATURE
+  desttrans = mailbox_transaction_begin(box, MAILBOX_TRANSACTION_FLAG_EXTERNAL);
+#else
+  char reason[256];
+  memset(reason, '\0', sizeof(reason));
+  desttrans = mailbox_transaction_begin(box, MAILBOX_TRANSACTION_FLAG_EXTERNAL, reason);
+#endif
+
+  search_ctx = mailbox_search_init(desttrans, search_args, NULL, static_cast<mail_fetch_field>(0), NULL);
+  mail_search_args_unref(&search_args);
+
+  struct message_size hdr_size, body_size;
+  struct istream *input = NULL;
+  while (mailbox_search_next(search_ctx, &mail)) {
+    save_ctx = mailbox_save_alloc(desttrans);  // src save context
+    EXPECT_NE(save_ctx, nullptr);
+
+    struct rbox_mail *r_mail = (struct rbox_mail *)mail;
+    struct rbox_storage *r_storage = (struct rbox_storage *)box->storage;
+
+    // delete mail.
+    r_storage->s->delete_mail(r_mail->mail_object);
+
+    int ret2 = mail_get_stream(mail, &hdr_size, &body_size, &input);
+    EXPECT_EQ(ret2, -1);
+    // EXPECT_NE(input, 0);
+    // EXPECT_NE(body_size.physical_size, (uoff_t)0);
+    // EXPECT_NE(hdr_size.physical_size, (uoff_t)0);
+    break;
+  }
+
+  if (mailbox_search_deinit(&search_ctx) < 0) {
+    FAIL() << "search deinit failed";
+  }
+
+  if (mailbox_transaction_commit(&desttrans) < 0) {
+    FAIL() << "tnx commit failed";
+  }
+
+  if (mailbox_sync(box, static_cast<mailbox_sync_flags>(0)) < 0) {
+    FAIL() << "sync failed";
+  }
+
+  ASSERT_EQ(0, (int)box->index->map->hdr.messages_count);
+
+  mailbox_free(&box);
+}
+
+TEST_F(StorageTest, deinit) {}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleMock(&argc, argv);
+  return RUN_ALL_TESTS();
+}
