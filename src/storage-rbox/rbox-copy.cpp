c669c943ca72e2514ba46867c0785d65f3c43d3d
Jan Radon <j.radon@tallence.com>
2018-11-12 11:12:10 +0100

    #207: fixed indentation

--- src/storage-rbox/rbox-copy.cpp
+++ src/storage-rbox/rbox-copy.cpp
@@ -320,24 +320,25 @@ static int rbox_mail_storage_try_copy(struct mail_save_context **_ctx, struct ma
       FUNC_END_RET("ret == -1, mail_save_copy_default_metadata failed");
       return -1;
     }
-T_BEGIN {
-    librmb::RadosStorage *rados_storage = !from_alt_storage ? r_storage->s : r_storage->alt;
-    if (ctx->moving != TRUE) {
-      if (copy_mail(ctx, rados_storage, rmail, &ns_src, &ns_dest) < 0) {
-        return -1;
+    T_BEGIN {
+      librmb::RadosStorage *rados_storage = !from_alt_storage ? r_storage->s : r_storage->alt;
+      if (ctx->moving != TRUE) {
+        if (copy_mail(ctx, rados_storage, rmail, &ns_src, &ns_dest) < 0) {
+          return -1;
+        }
       }
-    }
-    if (ctx->moving) {
-      if (move_mail(ctx, rados_storage, mail, &ns_src, &ns_dest) < 0) {
-        return -1;
+      if (ctx->moving) {
+        if (move_mail(ctx, rados_storage, mail, &ns_src, &ns_dest) < 0) {
+          return -1;
+        }
       }
-    }
 
-    index_copy_cache_fields(ctx, mail, r_ctx->seq);
-    if (ctx->dest_mail != NULL) {
-      mail_set_seq_saving(ctx->dest_mail, r_ctx->seq);
+      index_copy_cache_fields(ctx, mail, r_ctx->seq);
+      if (ctx->dest_mail != NULL) {
+        mail_set_seq_saving(ctx->dest_mail, r_ctx->seq);
+      }
     }
-}T_END;
+    T_END;
   }
 
   FUNC_END();
