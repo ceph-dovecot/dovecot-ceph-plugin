fedab40e071d430e08252ae5b2fa9068281416da
Jan Radon <j.radon@tallence.com>
2018-11-15 12:34:21 +0100

    sort -t with \t instead of ''

--- src/scripts/sort.sh
+++ src/scripts/sort.sh
@@ -1,1 +1,1 @@
-perl -n -e 'next if (/^doveadm/); if (/^(flags:\s*)(.*)/) {$_ = $1 . join(" ", sort split(" ",$2)) . "\n";}  print;' $1 | sed -e ':1;s/\f$/\f/;t 2;N;b 1;:2;y/\n/\t/' | sort -t  ' ' -k 1 > $2
+perl -n -e 'next if (/^doveadm/); if (/^(flags:\s*)(.*)/) {$_ = $1 . join(" ", sort split(" ",$2)) . "\n";}  print;' $1 | sed -e ':1;s/\f$/\f/;t 2;N;b 1;:2;y/\n/\t/' | sort -t  '	' -k 1 > $2
