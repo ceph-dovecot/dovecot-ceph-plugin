d2366a2ba65542e2d24772dbcc0b5ed26ee6f6cc
Jan Fabian Radon <j.radon@tallence.com>
2022-12-05 20:31:27 +0100

    Bugfix/355 fix gzip trailer for empty stream (#357)
    * #355 fix write gzip trailer even the stream is empty.
    
    * #355 minor improvements for quota messaging
    
    * #355 fix gzip trailer for empty stream

--- src/storage-rbox/rbox-save.cpp
+++ src/storage-rbox/rbox-save.cpp
@@ -784,9 +784,8 @@ int rbox_save_finish(struct mail_save_context *_ctx) {
 // clean stream if still open
 #if DOVECOT_PREREQ(2, 3)    
     int ret = 0;
-
-    if (r_ctx->ctx.data.output != r_ctx->output_stream && 
-    r_ctx->ctx.data.output != NULL) {
+    //#355 we need to write zipl trailer even the stream is empty!
+    if (r_ctx->ctx.data.output != r_ctx->output_stream ) {
       /* e.g. zlib plugin had changed this. make sure we
              successfully write the trailer. */
       i_info("check state2 %ld",r_ctx->ctx.data.output);
