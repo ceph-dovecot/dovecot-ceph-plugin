4cf3aa7f2fef5146685f2c2f11c8b1e44940c163
Jan Radon <j.radon@tallence.com>
2023-01-28 00:49:34 +0100

    fix ceph index size calculation

--- src/storage-rbox/rbox-save.cpp
+++ src/storage-rbox/rbox-save.cpp
@@ -657,9 +657,10 @@ int rbox_save_finish(struct mail_save_context *_ctx) {
         if( r_storage->config->get_object_search_method() == 2){
           // ceph config schalter an oder aus!
           r_storage->s->ceph_index_append(*r_ctx->rados_mail->get_oid());
-          uint64_t index_size = r_storage->s->ceph_index_size();
+          uint64_t index_size = r_storage->s->ceph_index_size();                  
+          double ceph_index_size_percent = ((double)index_size / (double)r_storage->s->get_max_object_size()) *(double)100.0;
           // WARN if index reaches 80% of max object size
-          if( ((index_size/r_storage->s->get_max_object_size()) * 100) > 80) {
+          if( ceph_index_size_percent > 0.80) {
             i_warning("ceph_index file(%d) close to exceed max_object size(%d), recalc index !", index_size, r_storage->s->get_max_object_size() );
           }
         }
